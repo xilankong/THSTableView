2357fc536d814e5068cc8aecf69779e0ecd56604
young.huang <young.huang@jfz.com>
2017-04-27 17:04:57 +0800

    first commit

---
+++ THSTableView/LeftCell.swift
@@ -0,0 +1,24 @@
+//
+//  LeftCell.swift
+//  THSTableView
+//
+//  Created by yanghuang on 2017/4/27.
+//  Copyright © 2017年 com.yang. All rights reserved.
+//
+
+import UIKit
+
+class LeftCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
